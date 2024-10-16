59241cd86e8b2b0bf94f7c16f869b0d26345d2d2
Menno <menno@deio.nl>
2024-10-16 10:02:28 +0200

    new screens

--- views/auth/login.php
+++ views/auth/login.php
@@ -7,60 +7,62 @@
 use webvimark\modules\UserManagement\components\GhostHtml;
 use webvimark\modules\UserManagement\UserManagementModule;
 use kartik\form\ActiveForm;
+use app\models\Site;
 
 use yii\helpers\Html;
 ?>
 
-<div class="container" id="login-wrapper">
-	<div class="row">
-		<div class="col-md-6 col-md-offset-6">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h1 class="panel-title"><?= \app\models\Site::ICON_LOGIN. ' '. UserManagementModule::t('front', 'Authorization') ?></h1>
-				</div>
-				<div class="panel-body">
-
-					<?php $form = ActiveForm::begin([
-                        'type'=> ActiveForm::TYPE_FLOATING,
-						'id'      => 'login-form',
-						'validateOnBlur'=>false,
-					]) ?>
-
-					<?= $form->field($model, 'username')
-						->textInput() ?>
-
-					<?= $form->field($model, 'password')
-						->passwordInput() ?>
-
-                    <div class="form-check form-switch">
- 					    <?= (isset(Yii::$app->user->enableAutoLogin) && Yii::$app->user->enableAutoLogin) ? $form->field($model, 'rememberMe')->checkbox(['class'=>'form-check-input','value'=>true]) : '' ?>
+    <div class="site-index">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h1 class="panel-title"><?= \app\models\Site::ICON_LOGIN. ' '. UserManagementModule::t('front', 'Authorization') ?></h1>
                     </div>
-					<?= Html::submitButton(\app\models\Site::ICON_LOGIN. ' '.
-						UserManagementModule::t('front', 'Login'),
-						['class' => 'btn btn-lg btn-primary btn-block']
-					) ?>
+                    <div class="panel-body">
 
-					<div class="row registration-block">
-						<div class="col-sm-6">
-							<?= GhostHtml::a(
-								UserManagementModule::t('front', "Registration"),
-								['/user-management/auth/registration']
-							) ?>
-						</div>
-						<div class="col-sm-6 text-right">
-							<?= GhostHtml::a('<i class="fa-solid fa-circle-question"></i> '.
-								UserManagementModule::t('front', "Forgot password ?"),
-								['/user-management/auth/password-recovery']
-							) ?>
-						</div>
-					</div>
+                        <?php $form = ActiveForm::begin([
+                            'type'=> ActiveForm::TYPE_FLOATING,
+                            'id'      => 'login-form',
+                            'validateOnBlur'=>false,
+                        ]) ?>
 
-					<?php ActiveForm::end() ?>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                        <?= $form->field($model, 'username')
+                            ->textInput() ?>
+
+                        <?= $form->field($model, 'password')
+                            ->passwordInput() ?>
+
+                        <div class="form-check form-switch">
+                            <?= (isset(Yii::$app->user->enableAutoLogin) && Yii::$app->user->enableAutoLogin) ? $form->field($model, 'rememberMe')->checkbox(['class'=>'form-check-input','value'=>true]) : '' ?>
+                        </div>
+                        <?= Html::submitButton(\app\models\Site::ICON_LOGIN. ' '.
+                            UserManagementModule::t('front', 'Login'),
+                            ['class' => 'btn btn-lg btn-primary btn-block']
+                        ) ?>
+                        <hr>
+
+                        <div class="row registration-block">
+                            <div class="col-sm-6">
+                                <?= GhostHtml::a(Site::ICON_REGISTER. ' '.
+                                    UserManagementModule::t('front', "Registration"),
+                                    ['/user-management/auth/registration']
+                                ) ?>
+                            </div>
+                            <div class="col-sm-6 text-right">
+                                <?= GhostHtml::a('<i class="fa-solid fa-circle-question"></i> '.
+                                    UserManagementModule::t('front', "Forgot password ?"),
+                                    ['/user-management/auth/password-recovery']
+                                ) ?>
+                            </div>
+                        </div>
+
+                        <?php ActiveForm::end() ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
 $css = <<<CSS
--- views/auth/registration.php
+++ views/auth/registration.php
@@ -1,49 +1,64 @@
 <?php
 
 use webvimark\modules\UserManagement\UserManagementModule;
-use yii\bootstrap5\ActiveForm;
+use kartik\form\ActiveForm;
 use yii\captcha\Captcha;
 use yii\helpers\Html;
+use app\models\Site;
+use app\models\User;
 
 /**
  * @var yii\web\View $this
  * @var webvimark\modules\UserManagement\models\forms\RegistrationForm $model
  */
 
-$this->title = UserManagementModule::t('front', 'Registration');
+$this->title = Yii::t('app', 'Registration for {sitename}',['sitename'=>Site::SITE_NAAM]);
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="user-registration">
+<div class="site-index">
+    <h1 class="panel-title"><?= Site::ICON_REGISTER. ' '. $this->title ?></h1>
 
-	<h2 class="text-center"><?= $this->title ?></h2>
+    <div class="alert alert-info">Geef de naam en e-mail van de patient met Diabetes type 1 op. Later kun  je andere familieleden uitnodigen om mee te doen.
+        <hr>
+        Je kunt <?=Site::SITE_NAAM . " ". Site::MONTHS_FREE ?> maanden gratis proberen zonder verplichtingen.
+    </div>
 
-	<?php $form = ActiveForm::begin([
-		'id'=>'user',
-		'layout'=>'horizontal',
-		'validateOnBlur'=>false,
-	]); ?>
+    <?php $form = ActiveForm::begin([
+        'id'=>'user',
+        'type'=> ActiveForm::TYPE_HORIZONTAL,
+        'validateOnBlur'=>false,
+    ]); ?>
 
-	<?= $form->field($model, 'username')->textInput(['maxlength' => 50, 'autocomplete'=>'off', 'autofocus'=>true]) ?>
+    <?= $form->field($model, 'username')->textInput(['maxlength' => 50, 'autocomplete'=>'off', 'autofocus'=>true]) ?>
 
-	<?= $form->field($model, 'password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
+    <?= $form->field($model, 'firstname')->textInput(['maxlength' => 50, 'autocomplete'=>'off']) ?>
+    <?= $form->field($model, 'lastname')->textInput(['maxlength' => 50, 'autocomplete'=>'off']) ?>
 
-	<?= $form->field($model, 'repeat_password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
+    <?= $form->field($model, 'family_name')->textInput(['maxlength' => 50, 'autocomplete'=>'off']) ?>
+    <?= $form->field($model, 'org_id')->dropDownList(User::getOrganisaties()) ?>
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
 
-	<?= $form->field($model, 'captcha')->widget(Captcha::className(), [
-		'template' => '<div class="row"><div class="col-sm-2">{image}</div><div class="col-sm-3">{input}</div></div>',
-		'captchaAction'=>['/user-management/auth/captcha']
-	]) ?>
 
-	<div class="form-group">
-		<div class="col-sm-offset-3 col-sm-9">
-			<?= Html::submitButton(
-				'<span class="glyphicon glyphicon-ok"></span> ' . UserManagementModule::t('front', 'Register'),
-				['class' => 'btn btn-primary']
-			) ?>
-		</div>
-	</div>
+    <?= $form->field($model, 'repeat_password')->passwordInput(['maxlength' => 255, 'autocomplete'=>'off']) ?>
+    <div class="alert alert-info">Het wachtwoord moet aan de volgende regels voldoen: <b>minimaal 8 tekens, minimaal een hoofdletter en een kleine letter.</b></div>
 
-	<?php ActiveForm::end(); ?>
+    <?= $form->field($model, 'captcha')->widget(Captcha::className(), [
+        'template' => '<div class="row"><div class="col-sm-2">{image}</div><div class="col-sm-3">{input}</div></div>',
+        'captchaAction'=>['/user-management/auth/captcha']
+    ]) ?>
+    <?= $form->field($model, 'accoord')->checkbox(['label'=>Yii::t('app','Ik ben accoord met de {voorwaarden} van Simpeld.nl',['voorwaarden'=>html::a('Voorwaarden',['/voorwaarden/index'])])]) ?>
+    <?= $form->field($model, 'gelezen')->checkbox(['label'=>Yii::t('app','Ik heb de {disclaimer} gelezen en begrepen',['disclaimer'=>html::a('Disclaimer',['/site/disclaimer'])])]) ?>
+
+    <div class="form-group">
+        <div class="col-sm-offset-3 col-sm-9">
+            <?= Html::submitButton(
+                '<span class="glyphicon glyphicon-ok"></span> ' . UserManagementModule::t('front', 'Register'),
+                ['class' => 'btn btn-primary']
+            ) ?>
+        </div>
+    </div>
+
+    <?php ActiveForm::end(); ?>
 
 </div>
